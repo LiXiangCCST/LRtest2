d6c24430d243cf912d51f5a1c06c7503140cb84f
LiXiangCCST <lixiang_ccst@jlu.edu.cn>
2021-07-26 18:07:20 +0800

    first commit

---
+++ test1_4.py
@@ -0,0 +1,11 @@
+import torch
+
+w = torch.tensor([1.], requires_grad=True)
+x = torch.tensor([2.], requires_grad=True)
+# y=(x+w)*(w+1)
+a = torch.add(x, w)
+b = torch.add(w, 1)
+y = torch.mul(a, b)
+# y求导
+y.backward()
+print(w.grad)
